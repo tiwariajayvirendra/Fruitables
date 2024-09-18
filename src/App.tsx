15948dcca617485209c1c9a107ae544f81636f59
AmanJotKN <amanjot@dice-academy.com>
2024-09-18 13:41:14 +0530

    designing all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Router from './router/Router'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
+    <>
+    <Header/>
    <Router/>
+   <Footer/>
+   </>
   )
 }
 
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Homepage from '../pages/Homepage'
+import Cart from '../pages/Cart'
+import Shop from '../pages/Shop'
+import Checkout from '../pages/Checkout'
+import Shopdetail from '../pages/Shopdetail'
+import Thanks from '../pages/Thanks'
+import Contact from '../pages/Contact'
 const Router = () => {
   return (
     <Routes>
 <Route path="/" element={<Homepage/>}/>
+<Route path="/cart" element={<Cart/>}/>
+<Route path="/shop" element={<Shop/>}/>
+<Route path="/checkout" element={<Checkout/>}/>
+<Route path="/shopdetails" element={<Shopdetail/>}/>
+<Route path="/thanks" element={<Thanks/>}/>
+<Route path="/contact" element={<Contact/>}/>
     </Routes>
   )
 }
